cf22969277fa2425b80696003ec81137cfb18f85
James Belta <james.belta@gmail.com>
2020-04-22 10:13:57 -0600

    Startes Recipe class. Passed assert instance test

---
+++ lib/recipe.rb
@@ -0,0 +1,7 @@
+class Recipe
+  attr_reader :dish
+
+  def initialize(dish)
+    @dish = dish
+  end
+end
---
+++ test/recipe_test.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ingredient'
+require './lib/recipe'
+
+class RecipeTest < Minitest::Test
+
+  def test_it_exist
+    recipe1 = Recipe.new("Mac and Cheese")
+    assert_instance_of Recipe, recipe1
+  end
+
+end
