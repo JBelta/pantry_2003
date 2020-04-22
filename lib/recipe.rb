61b6e8aa1523dd5a5d7cecb4dbeb9a162243a183
James Belta <james.belta@gmail.com>
2020-04-22 10:23:21 -0600

    Added assertions to attributes test. Edit Recipe class' instance variables and attr reader

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,7 +1,9 @@
 class Recipe
-  attr_reader :name
+  attr_reader :name, :ingredients, :ingredients_required
 
   def initialize(name)
     @name = name
+    @ingredients = Array.new
+    @ingredients_required = Hash.new(0)
   end
 end
