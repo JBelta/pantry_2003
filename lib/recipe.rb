977748174221de70f540c1869a3f7f6cf029cd1b
James Belta <james.belta@gmail.com>
2020-04-22 10:44:08 -0600

    Add ingredient method and test pass

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -6,4 +6,13 @@ class Recipe
     @ingredients = Array.new
     @ingredients_required = Hash.new(0)
   end
+
+  def add_ingredient(food, amount)
+    (@ingredients_required[food] += amount)
+    #couldn't we make this a module?
+    if @ingredients.any?(food) == false
+      @ingredients << food
+    end
+  end
+
 end
